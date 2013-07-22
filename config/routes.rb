be166ad9230576b355019f224ec2e9d00065b364
Zafer CAKMAK <mzcakmak@gmail.com>
2013-07-23 01:15:48 +0300

    adds some pages, changes some links

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 JsonifyMe::Application.routes.draw do
+  get "dashboard/show"
+  get "dashboard/settings"
   devise_for :users
   get "home/index"
-  match '/:username' => 'home#me', via: [:get], defaults: {format: :json}
+  match '/:username' => 'home#me', via: [:get], defaults: {format: :json}, as: :me
+
+  authenticated :user do
+    root :to => "dashboard#show", as: :authenticated_root
+  end
 
   root 'home#index'
 
