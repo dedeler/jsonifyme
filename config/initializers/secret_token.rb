a5812e009c2f3d5584dd3b98c4de9a33a571f612
Zafer CAKMAK <mzcakmak@gmail.com>
2013-07-22 17:30:28 +0300

    initial commit.
    - adds mongoid as datasource
    - adds devise as authentication module

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JsonifyMe::Application.config.secret_key_base = '15dba556d0af439f0f454e0e5da354fd2f319d174f0e7659632655d90f556c8747ab4e5b6532fd4ddd265dc090e025ca1bace6c4451b7c508d353806b40e4988'
